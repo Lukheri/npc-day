8df3227c36347f920e5d81f9f6ad5c22ff73b5b5
Luke Heri P. Contrivida <88194722+Lukheri@users.noreply.github.com>
2024-01-26 18:06:51 +0800

    Updated search filter
    Centered the search filter and added some styling

--- packages/nextjs/components/spark/NFTPicker.tsx
+++ packages/nextjs/components/spark/NFTPicker.tsx
@@ -33,7 +33,7 @@ export const NFTPicker = ({ address, className = "" }: TNFTPickerProps) => {
 
   if (!address || loading || nfts === null) {
     return (
-      <div className="animate-pulse flex space-x-4">
+      <div className="animate-pulse flex justify-center space-x-4">
         <div className="rounded-md bg-slate-300 h-6 w-6"></div>
         <div className="flex items-center space-y-6">
           <div className="h-2 w-28 bg-slate-300 rounded"></div>
@@ -132,15 +132,46 @@ export const NFTPicker = ({ address, className = "" }: TNFTPickerProps) => {
   };
 
   return (
-    <div className={className}>
-      <input
-        type="text"
-        placeholder="Search by name"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
+    <div className={`${className}`}>
+      <div className="w-full flex justify-center">
+        <div className="relative w-1/2">
+          <label htmlFor="Search" className="sr-only">
+            Search
+          </label>
 
-      <div>NFTs found: {filteredNFTs?.length}</div>
+          <input
+            type="text"
+            id="Search"
+            placeholder="Search by name"
+            value={searchTerm}
+            onChange={e => setSearchTerm(e.target.value)}
+            className="w-full rounded-md border-gray-200 py-2.5 pe-10 shadow-sm sm:text-sm"
+          />
+
+          <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+            <button type="button" className="text-gray-600 hover:text-gray-700">
+              <span className="sr-only">Search</span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </button>
+          </span>
+        </div>
+      </div>
+
+      <div className="text-center">NFTs found: {filteredNFTs?.length}</div>
       {
         // show dialog with selected NFT
         selectedNFT && (
